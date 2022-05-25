b60eed225cfff15ebdc48b0a33d7b3839ff6eb6d
Muhab Tomoum <muhabtomoum@gmail.com>
2022-05-24 23:53:57 -0700

    add new script

---
+++ PowerShell/ps_installs.ps1
@@ -0,0 +1,6 @@
+
+# Install Pretty icons
+Install-Module -Name Terminal-Icons -Repository PSGallery
+
+# Install pre release for list view of predictions
+Install-Module PSReadLine -AllowPrerelease -Force
