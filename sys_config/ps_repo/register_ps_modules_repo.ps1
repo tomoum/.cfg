5584f82bbeb27fcf415259502f812c4637e9b02c
Muhab Tomoum <muhabtomoum@gmail.com>
2022-08-22 12:13:14 -0700

    add ps repo registration script

---
+++ sys_config/ps_repo/register_ps_modules_repo.ps1
@@ -0,0 +1,18 @@
+<#
+.SYNOPSIS
+add local powershell repository to registered trusted PSRepository and
+upgrade Nuget.exe
+#>
+
+. $PSScriptRoot\common.ps1
+
+Write-Host Updating NuGet Package -ForegroundColor Green
+Invoke-WebRequest -Uri https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile "$env:LOCALAPPDATA\Microsoft\Windows\PowerShell\PowerShellGet\NuGet.exe"
+
+Write-Host Adding Repo Path: "$LocalRepoPath" to trusted repositories -ForegroundColor Green
+Register-PSRepository -Name "$LocalRepoName" `
+    -SourceLocation "$LocalRepoPath" `
+    -PublishLocation "$LocalRepoPath" `
+    -InstallationPolicy 'Trusted'
+
+Get-PSRepository
